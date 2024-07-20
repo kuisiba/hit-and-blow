ac368f2a3a8e61f7b097b557b916a11389faf39b
kuisiba <kuisiba@gmail.com>
2024-07-20 23:04:26 +0900

    fix: import order

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Form } from "antd";
 import { useEffect, useState } from "react";
 import { GuessesComponent } from "./components/GuessesComponent";
 import {
+	Description,
 	StyledButton,
 	StyledCard,
-	Description,
 	StyledFormItem,
 	StyledInput,
 } from "./components/Styled";
