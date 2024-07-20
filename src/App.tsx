bb081681ca8d0eddc2a147358e989630d1819278
kuisiba <kuisiba@gmail.com>
2024-07-20 15:38:05 +0900

    feat: ガワ

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { Form } from "antd";
+import {
+	StyledButton,
+	StyledCard,
+	StyledDiv,
+	StyledFormItem,
+	StyledInput,
+} from "./Styled";
+
 const App = () => {
-	return <></>;
+	return (
+		<>
+			<StyledCard>
+				<Form>
+					<StyledFormItem>
+						<StyledInput placeholder="4桁の数字を入力" />
+					</StyledFormItem>
+					<StyledFormItem>
+						<StyledButton type="primary" htmlType="submit">
+							決定
+						</StyledButton>
+					</StyledFormItem>
+				</Form>
+				<StyledDiv color="#377e22">位置と数字があっている</StyledDiv>
+				<StyledDiv color="#958129">位置が間違っている</StyledDiv>
+				<StyledDiv color="#5c0e09">位置と数字が間違っている</StyledDiv>
+			</StyledCard>
+		</>
+	);
 };
 
 export default App;
