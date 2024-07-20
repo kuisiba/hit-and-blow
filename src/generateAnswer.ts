8c94ab8521c9020eb4412be25e3bc518f074cdb1
kuisiba <kuisiba@gmail.com>
2024-07-20 17:26:58 +0900

    feat: generateAnswer()関数

---
+++ src/generateAnswer.ts
@@ -0,0 +1,12 @@
+export const generateAnswer = (): number[] => {
+	const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	const answer = [];
+
+	while (answer.length < 4) {
+		const randomIndex = Math.floor(Math.random() * numbers.length);
+		const randomNumber = numbers.splice(randomIndex, 1)[0];
+		answer.push(randomNumber);
+	}
+
+	return answer;
+};
